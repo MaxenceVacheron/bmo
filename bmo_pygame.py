4a9eb3d76c9c5cc8ae4d01a7e3fccff349856040
Maxence Vacheron <vacheronmaxence@gmail.com>
2026-02-08 17:39:37 +0100

    Proba

--- bmo_pygame.py
+++ bmo_pygame.py
@@ -639,7 +639,7 @@ def update_face():
             state["middle_finger_timer"] = now + random.uniform(5.0, 10.0)
         elif now > state["middle_finger_timer"]:
             # 20% chance to show middle finger
-            if random.random() < 0.2:
+            if random.random() < 0.8:
                 state["show_middle_finger"] = True
                 state["middle_finger_end_time"] = now + 2.0  # Show for 2 seconds
             state["middle_finger_timer"] = now + random.uniform(5.0, 10.0)
